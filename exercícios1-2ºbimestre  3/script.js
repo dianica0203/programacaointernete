0dd5ba57341a6a40ba60ec7d05a2ca0eb54d17ad
dianica0203 <dianica20305@edu.unipar.br>
2025-05-14 20:47:13 -0300

    Add files via upload

---
+++ exercícios1-2ºbimestre  3/script.js
@@ -0,0 +1,4 @@
+let inputinfumsaldo = document.querySelector("#inputinfumsaldo")
+let inputimprimir =document.querySelector("#inputimpr")
+let btmultiplicar = document.querySelector("#btmultiplicar")
+let h3saldoreajuste = Document.querySelector("#h3saldorajuste")
