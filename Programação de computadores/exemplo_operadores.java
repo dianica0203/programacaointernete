fd3b4515c790fc8168979f10abbd6cbf7908a127
dianica0203 <dianica20305@edu.unipar.br>
2025-08-19 19:31:11 -0300

    Add files via upload

---
+++ Programação de computadores/exemplo_operadores.java
@@ -0,0 +1,46 @@
+class exemplo_operadores{
+
+public static void main(String[] args) {
+
+
+    int A;
+    int B;
+    A= 24;
+    B= 7;
+
+System.out.println( "Resultados ds oeperaões entre A e B");
+System.out.println("A:" +A +"/nB:" + B);
+
+    int soma ;
+    soma = A+B;
+    System.out.println("soma:" + soma);
+
+    
+    int subtracao;
+    subtracao = A - B;
+    System.out.println("subtracao" + subtracao);
+
+    int multiplicacao;
+multiplicacao = A * B;
+System.out.println("multiplicacao" + multiplicacao);
+
+     double divisao;
+
+divisao = A / B;
+System.out.println("divisao" + divisao);
+
+    double resto_divisao;
+resto_divisao = A % B;
+System.out.println("resto_divisao" + resto_divisao);
+ 
+
+System.out.println("ResultadoS das operações Relacionais entre A e B");
+System.out.println("A:" +A +"/nB:" + B);
+System.out.println("A<B:" + (A<B) );
+
+}
+
+
+
+
+}
---
+++ Programação de computadores/teste1.java
@@ -0,0 +1,17 @@
+class teste1 {
+
+public static void  main(String[] args){
+
+System.out.println("Ola Mundo!/n/nEsse é meu primeiro programa em java");
+
+int idade = 35;
+idade = 15;
+System.out.println("idade:  " + idade);
+
+//declarar uma variavél do tipo string chamada nome:
+
+
+
+}
+
+}
