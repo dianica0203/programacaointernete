31b7bfbb1e604429533a9416e2710f02d2b1edd5
dianica0203 <dianica20305@edu.unipar.br>
2025-08-26 21:18:04 -0300

    Add files via upload

--- Programação de computadores/teste1.java
+++ Programação de computadores/teste1.java
@@ -10,7 +10,23 @@ System.out.println("idade:  " + idade);
 
 //declarar uma variavél do tipo string chamada nome:
 
+String nome;
 
+nome = "nome: Dianica";
+
+System.out.println(nome);
+
+ //declarar uma variável do tipo boolean chamada status_aluno:
+
+ boolean status_aluno;
+ status_aluno = true;
+ System.out.println(status_aluno);
+
+ //declarar uma variável do tipo char chamada periodo;
+
+ char periodo;
+ periodo = '2' ;
+ System.out.println(periodo);
 
 }
 
