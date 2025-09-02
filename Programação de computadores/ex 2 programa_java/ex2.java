ab020d8bd849ce7642f86b47a97e480ed83ff566
dianica0203 <dianica20305@edu.unipar.br>
2025-09-02 20:42:47 -0300

    Add files via upload

---
+++ Programação de computadores/ex 2 programa_java/ex2.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+class ex2 {
+
+
+    public static void main(String[]args){
+
+
+ Scanner entrada = new Scanner(System.in);
+
+String  nome;
+int idade;
+String Gênero;
+String corfavorita;
+String esporte;
+
+System.out.println("nome:" );
+nome = entrada.next();
+
+System.out.println("idade:");
+idade = entrada.nextInt();
+
+
+System.out.println("Gênero:");
+Gênero = entrada.next();
+
+System.out.print("corfavorita:");
+corfavorita =entrada.next();
+
+System.out.println("esporte:");
+esporte = entrada.next();
+
+}
+
+}
