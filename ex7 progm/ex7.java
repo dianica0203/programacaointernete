1a0d8d91e3181540b02d9f34fc6d0a182092ddbc
dianica0203 <dianica20305@edu.unipar.br>
2025-09-02 21:32:01 -0300

    Add files via upload

---
+++ ex7 progm/ex7.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+class ex7 {
+    public static void main(String[]args){
+ /*
+ Faça um programa que peça ao usuário dois números (num1 e num2) e depois exiba o resultado das operações relacionais maior que, menor que, igual e diferente
+
+  */   
+
+int num1, num2;
+Scanner s = new  Scanner(System.in);
+
+
+
+System.out.print("Informe o primeiro número:");
+num1 = s.nextInt();
+
+System.out.print("Informe o segundo número:");
+num2 = s.nextInt();
+
+
+
+System.out.print("Resultado dos comparação entre os numeros informados:");
+System.out.println("num1 > num2:");
+System.out.println("num1 >= num2");
+System.out.println("num1 > num2:");
+System.out.println("num1 > num2:");
+System.out.println("num1 > num2:");
+    }
+}
