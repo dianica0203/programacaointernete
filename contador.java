03730ef27e7be76d2dab2ee2e1497a98a6582086
dianica0203 <dianica20305@edu.unipar.br>
2025-09-18 21:15:26 -0300

    Add files via upload

---
+++ contador.java
@@ -0,0 +1,31 @@
+public class contador {
+    
+    public static void main(String[] args) {
+        //int cont;
+       // cont = 0;
+ //while (cont < 5){
+       // System.out.println("contador" + cont);
+       // cont++;
+
+
+       int  cont;
+       cont = 1;
+
+ while (cont < 5){
+        System.out.println("contador" + cont);
+        cont++;
+
+        System.out.println("exemplo com repita (do):");
+        cont = 0 ;
+        do { 
+            System.out.println("contador:" + cont);
+        } while (cont < 5);
+System.out.println("Exemplo com para (for):");
+for(int i=0; i < 5; i++){
+    System.out.println("contaor:" + i);
+}
+        
+
+    }
+    }
+}
