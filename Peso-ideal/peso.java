a2f16f33c590fb11999b774d24380f31e0243281
dianica0203 <dianica20305@edu.unipar.br>
2025-09-23 20:17:13 -0300

    Add files via upload

---
+++ Peso-ideal/peso.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+
+class peso {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+
+double altura;
+double peso;
+double pesoideal;
+int sexo;
+
+System.out.println("digite sua altura (em metro):");
+altura = sc.nextDouble();
+System.out.println("digite seu peso (em kg):");
+peso = sc.nextDouble();
+System.out.println("digite seu sxo (M/F):");
+sexo = sc.nextInt();
+
+
+         if 
+
+         (sexo.equalsIgnoreCase("M")) {
+
+    pesoideal = (72.7  * altura) - 58;
+    
+}else{
+    pesoideal = (62.1 * altura) - 44.7;
+}
+System.out.println("peso ideal:" + pesoideal);
+if(peso <pesoideal){
+    System.out.println("voce está abaixo do peso ideal.");
+}else if ( peso > pesoideal)  {
+    System.out.println("voce está aciam do peso ideal.");
+}else{
+    System.out.println("voce está no peso ideal!");
+}
+
+sc.close();
+
+    }
+    
+    
+}
+    
