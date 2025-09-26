1cf6d23eb05f1067b98e196e0462dabc0fae3971
dianica0203 <dianica20305@edu.unipar.br>
2025-09-25 21:49:28 -0300

    B

---
+++ lista_ex10.java
@@ -0,0 +1,22 @@
+
+import java.util.Scanner;
+
+
+public class lista_ex10 {
+    
+    public static void main(String[] args) {
+        
+        //Construa um programa que exiba os valores entre dois números informados pelo usuário.//
+
+        int num1;
+        int num2;
+        Scanner S = new Scanner(System.in);
+
+System.out.println("Informa o premeiro número");
+num1 = S.nextInt();
+
+System.out.println("Informe o segundo número");
+num2 = S.nextInt();
+    }
+
+}
